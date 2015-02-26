faea6c6b8dd17026dee515a738e06e76aee9811c
harryfhliu <harryfhliu@outlook.com>
2015-02-26 19:07:56 +0800

    Day 3
    Day 3 exes

---
+++ ex5.py
@@ -0,0 +1,17 @@
+my_name = "Harry LIU"
+my_age = 30 # that's true
+my_height = 71 # inches
+my_weight = 200 # or guess that
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print "Let's talk about %s" % my_name
+print "He's %d inches tall." % my_height
+print "He's %d pounds heave." % my_weight
+print "Actually that's too heavy."
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth
+
+# this line is tricky, try to get it exactly right
+print "If I add %d, %d, and %d I get %d" % (my_age, my_height, my_weight, my_age + my_height + my_weight)
